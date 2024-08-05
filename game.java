9d50c925b9e896f0cb3702b7527b0df9c8d84a94
Marz <mewpudit@gmail.com>
2024-08-05 17:42:00 +0700

    first commit

---
+++ game.java
@@ -0,0 +1,15 @@
+
+
+public class game{
+    public static void main(String[] args){
+
+        System.out.println("test");
+
+
+
+
+
+
+    }
+
+}
---
+++ xobot.java
@@ -0,0 +1,22 @@
+public class xobot {
+    
+
+    private int[][] state;
+    private int initialTurn;
+
+
+    public xobot(int t){
+        this.initialTurn = t;
+    }
+
+
+
+    public void observe(int[][] state){
+        this.state = state;
+    }
+
+
+
+
+
+}
