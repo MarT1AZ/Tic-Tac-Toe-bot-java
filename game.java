d1c3da9cb1aa994897ab82719ff150f1026d4a76
Marz <mewpudit@gmail.com>
2024-08-05 17:52:44 +0700

    test-commit

--- game.java
+++ game.java
@@ -1,6 +1,11 @@
 
 
 public class game{
+
+    public static int[][] board;
+
+    
+
     public static void main(String[] args){
 
         System.out.println("test");
--- xobot.java
+++ xobot.java
@@ -15,6 +15,10 @@ public class xobot {
         this.state = state;
     }
 
+    public void calculateNextMove(){
+        
+    }
+
 
 
 
